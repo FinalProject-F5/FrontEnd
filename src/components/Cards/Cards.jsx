a53692db282097f536989ba335ad6293158d1078
Mariuxi <catmaluci@hotmail.com>
2025-05-29 18:45:43 +0200

    fixing footer

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,30 +1,24 @@
-import React from "react";
-import imageTemporal2 from "../../assets/imageTemporal2.png";
-const cards = [
-  {
-    title: "Arte",
-    description: "Descubre experiencias artísticas únicas y creativas.",
-    img: imageTemporal2,
-  }
-];
 
-export default function Cards() {
+import React from "react";
+
+
+export default function Cards({ title, category, location, img }) {
   return (
-    <div className="flex flex-wrap gap-6 justify-center py-8">
-      {cards.map((cat) => (
-        <div key={cat.title} className="card bg-base-100 w-96 shadow-sm">
-          <figure>
-            <img src={cat.img} alt={cat.title} />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">{cat.title}</h2>
-            <p>{cat.description}</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Ver más</button>
-            </div>
-          </div>
+   
+    <div className="card bg-base-100 w-full shadow-sm">
+      <figure>
+        <img src={img} alt={title} className="w-full h-48 object-cover" /> 
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-neutral">{title}</h2>
+
+        <p className="text-secondary">{category}</p>
+       
+        <p className="text-primary font-semibold text-sm mt-2">{location}</p> 
+        <div className="card-actions justify-end mt-4"> 
+          <button className="btn btn-primary">Read more</button>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
