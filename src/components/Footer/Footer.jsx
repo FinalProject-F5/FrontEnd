7f983311d035e883442199339ff6539c7f4a0c69
Mariuxi <catmaluci@hotmail.com>
2025-05-29 18:55:36 +0200

    fixing footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,23 +4,23 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer 
-      className="footer items-center justify-between text-base-content p-6 flex flex-row"
-      style={{ backgroundColor: "#B3C7BB" }} 
+    <footer
+      className="footer items-center justify-between text-base-content p-4 flex flex-col gap-y-2 md:flex-row md:gap-x-4" 
+      style={{ backgroundColor: "#B3C7BB" }}
     >
-     
-      <nav className="flex gap-4 ml-4">
+      <nav className="flex flex-row gap-4 justify-center w-full md:w-auto">
         <a className="link link-hover">Home</a>
         <a className="link link-hover">Search</a>
         <a className="link link-hover">Add Experiences</a>
       </nav>
-      <aside className="flex items-center gap-2">
+
+      <aside className="flex items-center gap-2 w-full md:w-auto justify-center md:justify-start">
         <p className="font-bold">
           Travel4Real © {currentYear} All rights reserved
         </p>
       </aside>
-
-      <nav className="flex gap-4 mr-4">
+      
+      <nav className="flex flex-row gap-4 w-full md:w-auto justify-center md:justify-end">
         <a>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
             <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
