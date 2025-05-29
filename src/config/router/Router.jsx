f6c9bf5ebdb0571c02be9c5c8007415caf1f7646
Mariuxi <catmaluci@hotmail.com>
2025-05-29 19:17:15 +0200

    layout login

--- src/config/router/Router.jsx
+++ src/config/router/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/homePage/HomePage";
 import LandingPage from "../../pages/landingPage/LandingPage";
+import Login from "../../pages/Login/Login";
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/HomePage" element={<HomePage/>} />
+        <Route path="/Login" element={<Login/>} />
      
       </Routes>
     </BrowserRouter>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,58 @@
 import React from "react";
+import Header from "../../components/header/header";
+import Footer from "../../components/Footer/Footer";
 
 export default function Login() {
   return (
-    <div>
-      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="fieldset-legend">Login</legend>
+    <>
+      <Header />
+    <div
+      className="min-h-screen flex items-center justify-center bg-cover bg-center"
+      style={{
+        backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)", 
+      }}
+    >
+     
+      <div className="card w-96 bg-base-100 shadow-xl p-4">
+        <div className="card-body items-center text-left">
+          <h2 className="card-title text-4xl font-bold text-primary mb-6">Login</h2> 
 
-        <label className="label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
+       
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="input input-bordered w-full"
+            />
+          </div>
 
-        <label className="label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
-
-        <button className="btn btn-neutral mt-4">Login</button>
-      </fieldset>
+          <div className="form-control w-full max-w-xs mb-4">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input input-bordered w-full"
+            />
+          </div>
+       
+          <button className="btn btn-primary w-full text-lg py-3 rounded-lg mb-4">Login</button>
+          
+         
+          <div className="text-sm">
+            <p className="mb-1">Not registered yet?{' '}
+              <a href="#" className="link link-hover text-primary font-semibold">Sign up here</a>
+            </p>
+            <a href="#" className="link link-hover text-neutral-content">Forgot password?</a>
+          </div>
+        </div>
+      </div>
     </div>
+          <Footer />
+        </>
   );
 }
