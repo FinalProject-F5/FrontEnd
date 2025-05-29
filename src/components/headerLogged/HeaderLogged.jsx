7bf90ccb6d6cfb1e2668d21ea6369a21c82a5d2d
Mariuxi <catmaluci@hotmail.com>
2025-05-29 23:02:16 +0200

    add avatar in mobile

--- src/components/headerLogged/HeaderLogged.jsx
+++ src/components/headerLogged/HeaderLogged.jsx
@@ -12,7 +12,8 @@ export default function HeaderLogged() {
 
       <div className="flex-none flex items-center md:order-last">
   
-        <div className="hidden md:flex items-center mr-4"> 
+        {/* MODIFICACIÓN AQUÍ: Se eliminan las clases 'hidden' y 'md:flex' */}
+        <div className="flex items-center mr-4"> 
           <div className="btn btn-ghost btn-circle avatar"> 
             <div className="w-10 rounded-full">
               <img alt="User Avatar" src="src/assets/avatar_placeholder.png" />
