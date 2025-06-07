c67500b2241dc721f86494ee6aa161d80678fc92
Mariuxi <catmaluci@hotmail.com>
2025-06-07 16:10:42 +0200

    add decoding jwt token to check if login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import { createContext, useContext, useState, useEffect } from "react";
 
 
 const AuthContext = createContext(null);
@@ -6,8 +7,21 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  return (
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      try {
+        const decodedUser = jwtDecode(user.token);
+        setUser(decodedUser);
+      } catch (error) {
+        localStorage.removeItem('token');
+      } finally {
+        setLoading(false);
+      }
+    }
+  }, []);
 
+  return (
     <AuthContext.Provider value={{ user, loading }}>
       {children}
     </AuthContext.Provider>
