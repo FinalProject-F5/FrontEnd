8482af7063d469e180b8a987fdcdc3d77356d303
Mariuxi <catmaluci@hotmail.com>
2025-06-17 11:10:42 +0200

    fix color cards in my experiences

--- src/pages/MyExperiences/MyExperiences.jsx
+++ src/pages/MyExperiences/MyExperiences.jsx
@@ -62,7 +62,7 @@ export default function MyExperiences() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-6xl mx-auto py-8 px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-6xl mx-auto py-8 px-4 [filter:sepia(40%)]" >
         {myExperiences.map((exp) => (
           <Cards
             key={exp.id}
